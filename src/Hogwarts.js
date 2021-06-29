cc97ec9ef4659c4db302f48998d5ab98c99bdbe8
Roli <rroycehg@gmail.com>
2021-06-29 17:11:09 -0400

    Done

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,23 +1,28 @@
 import React from "react";
 import whoseHouse from "./houses/whoseHouse";
 import Hufflepuff from "./houses/Hufflepuff";
-import * as GryffFunctions from "./houses/Gryffindor";
+import { colors, values, gryffMascot } from "./houses/Gryffindor";
 
-console.log(GryffFunctions.colors);
+console.log(colors);
 // => 'Scarlet and Gold'
 
-GryffFunctions.gryffMascot();
+// gryffMascot();
 // => 'The Lion'
 
-// GryffFunctions.values();
+// values();
 // => Attempted import error
 
-export default function Hogwarts() {
+function Hogwarts() {
   whoseHouse();
 
   return (
     <div>
       <Hufflepuff />
+      {values()}
+      {gryffMascot()}
+
     </div>
   );
 }
+
+export default Hogwarts
